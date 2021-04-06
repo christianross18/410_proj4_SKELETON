911606a5430f4b081277327978a6773ca96ae317
christian <christian.ross.18@cnu.edu>
2021-04-06 19:37:27 -0400

    added non thread code

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,8 +1,10 @@
 #include <mutex>
 
 #include "../includes/baker.h"
+#include "../includes/externs.h"
 using namespace std;
-
+//ID is just a number used to identify this particular baker
+//(used with PRINT statements)
 Baker::Baker(int id):id(id)
 {
 }
@@ -10,11 +12,39 @@ Baker::Baker(int id):id(id)
 Baker::~Baker()
 {
 }
-
+//bake, box and append to anOrder.boxes vector
+//if order has 13 donuts there should be 2 boxes
+//1 with 12 donuts, 1 with 1 donut
 void Baker::bake_and_box(ORDER &anOrder) {
-	
+	int originial = anOrder.number_donuts;
+	while(anOrder.number_donuts>0){
+		Box b;
+		while(b.size()<12){
+			DONUT d;
+			b.addDonut(d);
+		}
+		anOrder.boxes.push_back(b);
+	}
+	anOrder.number_donuts=originial;
 }
-
+//as long as there are orders in order_in_Q then
+//for each order:
+//	create box(es) filled with number of donuts in the order
+//  then place finished order on order_outvector
+//  if waiter is finished (b_WaiterIsFinished) then
+//  finish up remaining orders in order_in_Q and exit
+//
+//You will use cv_order_inQ to be notified by waiter
+//when either order_in_Q.size() > 0 or b_WaiterIsFinished == true
+//hint: wait for something to be in order_in_Q or b_WaiterIsFinished == true
 void Baker::beBaker() {
+	while(!b_WaiterIsFinished){
+		while(!order_in_Q.empty()){
+			ORDER a=order_in_Q.front();
+			order_in_Q.pop();
+			bake_and_box(a);
+			order_out_Vector.push_back(a);
+		}
+	}
 	
 }
--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,22 +1,40 @@
 #include <string>
 #include "stdlib.h"
 
+#include "../includes/externs.h"
 #include "../includes/waiter.h"
 
 using namespace std;
-
+//ID is just a number used to identify this particular baker
+//(used with PRINT statements)
+//filename is what waiter reads in orders from
 Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
 }
 
 Waiter::~Waiter()
 {
 }
-
+//gets next Order from file_IO
+//if return == SUCCESS then anOrder
+//contains new order
+//otherwise return contains fileIO error
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
+	int i=myIO.getNext(anOrder);
+	if(i!=SUCCESS){
+		return FAIL;
+	}
 	return SUCCESS;
 }
-
+//contains a loop that will get orders from filename one at a time
+//then puts them in order_in_Q then signals baker(s) using cv_order_inQ
+//so they can be consumed by baker(s)
+//when finished exits loop and signals baker(s) using cv_order_inQ that
+//it is done using b_WaiterIsFinished
 void Waiter::beWaiter() {
+	ORDER a;
+	while(getNext(a) == SUCCESS){
+		order_in_Q.push(a);
+	}
 }
 
